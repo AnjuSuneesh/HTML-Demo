0fd44706bef331dc68d21a1415e9adf7b32e3aeb
anju123 <anjulakshmi@gmail.com>
2022-03-01 22:59:45 -0600

    modified

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+// (let a= 5 , b=10;
+// console.log(typeof age);
+// if(a<b){
+//     let c=50;
+// }
+
+// else{
+//     let c=100;
+// }
+// console.log(c);
+/* let user = "John";
+ user = "Mike";
+ console.log(user); */
+ //let userProfile;//Camelcase
+ const COLOR="red";
+ //COLOR = "BLUE";
+
+/* console.log(COLOR);
+ console.log(5=="5");
+ console.log(5==="5");
+ console.log("5"==="5");
+ console.log('5' ==="5");*/
+
+ let a=10;
+ 
